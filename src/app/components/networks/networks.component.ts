8fe037b0069607194b4229329ceca6cdfc646226
JuanMR <juanmr.093@gmail.com>
2024-08-02 18:52:13 -0300

    Actualizando banner y redes

--- src/app/components/networks/networks.component.ts
+++ src/app/components/networks/networks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { LanguageService } from 'src/app/services/language.service';
 import { ThemeService } from 'src/app/services/theme.service';
@@ -15,11 +15,17 @@ export class NetworksComponent implements OnInit {
   ) {}
   private themeSubscription!: Subscription;
   isDarkMode: boolean = false;
+  isDesktop: boolean = true;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
+  }
   ngOnInit() {
     this.themeSubscription = this.themeService.darkMode$.subscribe((isDark) => {
       this.isDarkMode = isDark;
     });
+    this.checkScreenSize();
   }
 
   ngOnDestroy() {
@@ -30,5 +36,8 @@ export class NetworksComponent implements OnInit {
 
   changeLanguage(language: string) {
     this.languageService.switchLanguage(language);
+  }
+  private checkScreenSize() {
+    this.isDesktop = window.innerWidth > 768;
   }
 }
